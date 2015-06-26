e65721e240247b8000d0ae1ca11c918f9471a140
Francois Faubert <francois.faubert@iprospect.com>
2015-06-26 12:01:19 -0400

    using another pdo file

--- src/PluginInitializer.php
+++ src/PluginInitializer.php
@@ -3,6 +3,7 @@
 namespace Strata\MuPlugin\Sqlite;
 
 use Strata\Strata;
+use Exception;
 
 /**
  * Sqlite Plugin initializer
@@ -11,10 +12,27 @@ class PluginInitializer {
 
     public static function initialize()
     {
-        if (defined("WPDB_DRIVER") && strtolower(WPDB_DRIVER) === 'sqlite') {
-            $path = array(Strata::getMUPluginsPath() . "wp-db-driver", "db.php");
-            require_once(implode(DIRECTORY_SEPERATOR, $path));
+        if (!defined('WP_ENV')) {
+            throw new Exception("Strata\MuPlugin\Sqlite can only run in a development environment.");
         }
+
+        // Turn this off by declaring use MySQL elsewhere
+        if (!defined('USE_MYSQL')) {
+
+            // Only load our configuration in dev mode.
+            if (strtolower(WP_ENV) === "development") {
+                define('USE_MYSQL', false);
+                if (!defined('DB_FILE')) {
+                    define('DB_FILE', 'development.sqlite3');
+                }
+                if (!defined('DB_DIR')) {
+                    define('DB_DIR', 'tmp');
+                }
+            } else {
+                define('USE_MYSQL', true);
+            }
+        }
+
     }
 
 }
